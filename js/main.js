b52c25dbd8f8ccec74d07767a50d6fbbad62d12f
Jonathan Somer <somer@mail.tau.ac.il>
2016-09-24 19:04:51 +0300

    a

--- js/main.js
+++ js/main.js
@@ -166,6 +166,8 @@ var usersLocatonMarker;
 var service;
 
 function initMap(){
+  document.getElementById('googleMapsError').remove();
+
   map = new google.maps.Map(document.getElementById('map') , {
     center : {lat: 32.0853, lng: 34.7818},
     zoom: 8
